5247ddbe369b8328f1e64cf95f3f53de41de3a3a
Debbroto Kumar Karmokar <136155528+debbrotokumark@users.noreply.github.com>
2024-06-16 20:30:12 +0600

    Create R - Opposite Task.cpp

---
+++ R - Opposite Task.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int a,b;
+	cin >> a;
+	for (int i = 1; i <= a; i++) {
+		cin>>b;
+        cout<<b/2<<" "<<b/2+b%2<<endl;
+
+
+
+
+
+	}
+
+}
